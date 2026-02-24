7acbb9245bf6ccc9552b24af9daaa4b261429e47
VowkStudio <studiovowk@gmail.com>
2026-02-24 15:33:00 -0300

    Updates

--- assets/Editor-DjmWtBWy.js
+++ assets/Editor-DjmWtBWy.js
@@ -1,4 +1,4 @@
-import{c as j,j as e,I as z,L as S,M as $,a as _,u as D,r as k,C as O}from"./index-Bb3UHN5w.js";/**
+import{c as j,j as e,I as z,L as S,M as $,a as _,u as D,r as k,C as O}from"./index-m2gzupc5.js";/**
  * @license lucide-react v0.544.0 - ISC
  *
  * This source code is licensed under the ISC license.
